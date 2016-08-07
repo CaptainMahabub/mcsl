7a74aac89912061b27d0d8dfa223ef6c8761c980
Mahabubur Rahman <mahabuburrahman.bd92@gmail.com>
2016-08-08 02:32:57 +0600

    monthly_balanc_sheet added

--- management/balance_sheet_management.php
+++ management/balance_sheet_management.php
@@ -1,5 +1,5 @@
 <a class="dropmenu" href="#"><i class="icon-folder-close-alt"></i><span class="hidden-tablet">Balance Sheet</span>&nbsp; &nbsp;<span class="label label-important"> * </span></a>
 							<ul>
 								<li><a class="submenu" href="submenu.html"><i class="icon-file-alt"></i><span class="hidden-tablet"> Daily</span></a></li>
-								<li><a class="submenu" href="submenu2.html"><i class="icon-file-alt"></i><span class="hidden-tablet"> Monthly</span></a></li>
+								<li><a class="submenu" href="monthly_balanc_sheet.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> Monthly</span></a></li>
 							</ul>	
