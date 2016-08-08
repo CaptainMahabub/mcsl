94106ae2bb2a17d3f204ae37c35184059639bb3a
rubainamirza <rubainamirza@gmail.com>
2016-08-08 13:58:23 +0600

    edit

--- management/edit_deposite.php
+++ management/edit_deposite.php
@@ -1,91 +1,63 @@
-<?php
+
+
+ <?php
 require('db_config.php');
-?>
-
-
-<a class="dropmenu" href="#"><i class="icon-folder-close-alt"></i><span class="hidden-tablet">Account Management</span>&nbsp; &nbsp;<span class="label label-important"> 4 </span></a>
-							<ul>
-								<li><a class="submenu" data-toggle = "modal" data-target = "#editDeposite"><i class="icon-file-alt"></i><span class="hidden-tablet"> Edit Deposite</span></a></li>
-                                
-                                
-								<!--<li><a class="submenu" href="view_account.php"><i class="icon-file-alt"></i><span class="hidden-tablet"> View Account</span></a></li>-->
-                               
-							</ul>	
-                            
-                            
-                            
+?>                         
 <?php
 $id = $_GET['id'];
-$row = $mysqli->query("SELECT * FROM  deposite WHERE id = '$id'");
+$row = $mysqli->query("SELECT * FROM deposite_withdraw WHERE id = '$id'");
 $data = $row->fetch_array();
 
 ?>
 
-<!-- Edit Account -->
-<div class = "modal fade" id = "editDeposite" tabindex = "-1" role = "dialog" 
-   aria-labelledby = "myModalLabel" aria-hidden = "true">
+<form class = "form-inline" role = "form" name="account_form" method="post" action="update_deposite.php" enctype="multipart/form-data">
    
-   <div class = "modal-dialog">
-      <div class = "modal-content">
-         
-         <div class = "modal-header">
-            <button type = "button" class = "close" data-dismiss = "modal" aria-hidden = "true">
-               
-            </button>
-            
-            <h4 class = "modal-title" id = "myModalLabel">
-               Edit Deposite 
-            </h4>
-         </div>
-         
-         <div class = "modal-body">
-<form class = "form-inline" role = "form" name="account_form" method="post" action="update_deposite.php">
-   
-   <div class = "form-group">
+                            
+                            
+                            
+
+
+<div class = "form-group">
       <label class = "sr-only" for = "id"></label>
-      <input type = "hidden" name="id" class = "form-control" id = "id" value = "<?php echo $id;?>">
+      <input type = "hidden" name="id" class = "form-control" id = "id">
    </div>
+   
+   
    <div class = "form-group">
       <label class = "sr-only" for = "name">Account No</label>
-      <input type = "text" name="account_no" class = "form-control" id = "account_no" placeholder = "Name" value="<?php echo $data['account_no'] ?>">
+      <input type = "text" name="account_no" class = "form-control" id = "account_no" placeholder = "Account No" value="<?php echo $data['account_no'] ?>">
    </div>
+
+<div class = "form-group">
+
+<label class = "sr-only" for = "select">deposite or withdraw</label>
+     
+      <select name="select" class = "form-control" id = "select">
+         <option value="">Select Type</option>
+         <option value="deposite">deposite</option>
+         <option value="withdraw">withdraw</option>
+
+      </select>
+   </div>
+
    <div class = "form-group">
       <label class = "sr-only" for = "name">Date</label>
-      <input type = "date" name="date" class = "form-control" id = "date" placeholder = "date" value="<?php echo $data['date'] ?>">
+      <input type = "date" name="date" class = "form-control" id = "date" placeholder = "Date" value="<?php echo $data['date'] ?>">
    </div>
    <div class = "form-group">
-      <label class = "sr-only" for = "name">Tracking no</label>
-      <input type = "text" name="tracking_no" class = "form-control" id = "tracking_no" placeholder = "tracking_no" value="<?php echo $data['tracking_no'] ?>">
+      <label class = "sr-only" for = "name">Tracking Number</label>
+      <input type = "text" name="tracking_no" class = "form-control" id = "tracking_no" placeholder = "Tracking Number" value="<?php echo $data['tracking_no'] ?>">
    </div>
    <div class = "form-group">
       <label class = "sr-only" for = "name">Ammount</label>
-      <input type="text" name="ammount" class = "form-control" id = "ammount" placeholder = "ammount" value="<?php echo $data['ammount'] ?>">
+      <input type="text" name="ammount" class = "form-control" id = "ammount" placeholder = "Ammount" value="<?php echo $data['ammount'] ?>">
    </div>
    
    <div class = "form-group">
       <input class="btn btn-primary" type="submit" name="update" value="Update">
    </div>
+
+   
    
 </form>
-         </div>
          
-         <div class = "modal-footer">
-            <button type = "button" class = "btn btn-default" data-dismiss = "modal">
-               Close
-            </button>
-            
-            
-         </div>
-         
-      </div><!-- /.modal-content -->
-   </div><!-- /.modal-dialog -->
-   
-</div><!-- /.modal -->
-
-
-
-<script>
-   $(function () { $('#editDeposite').modal({
-      keyboard: true
-   })});
-</script>
