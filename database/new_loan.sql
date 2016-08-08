ba7b7296106b71515f11a8445ba5cc9837dd16b3
Umaiya Asma <asmaumaiya@gmail.com>
2016-08-08 12:01:48 +0600

    drop old new_loan.sql & import new_loan.sql

---
+++ database/new_loan.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 08, 2016 at 11:59 AM
+-- Server version: 10.1.8-MariaDB
+-- PHP Version: 5.6.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mcs_maindb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `new_loan`
+--
+
+CREATE TABLE `new_loan` (
+  `newloan_id` int(11) NOT NULL,
+  `account_no` varchar(50) NOT NULL,
+  `date` date NOT NULL,
+  `loan_amount` varchar(50) NOT NULL,
+  `installment` varchar(50) NOT NULL,
+  `grantors` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `new_loan`
+--
+
+INSERT INTO `new_loan` (`newloan_id`, `account_no`, `date`, `loan_amount`, `installment`, `grantors`) VALUES
+(1, '111455', '2016-08-04', '600000', '6', 'Wasrer'),
+(3, '123456', '2016-08-08', '800000', '6', 'Clinton');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `new_loan`
+--
+ALTER TABLE `new_loan`
+  ADD PRIMARY KEY (`newloan_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `new_loan`
+--
+ALTER TABLE `new_loan`
+  MODIFY `newloan_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
