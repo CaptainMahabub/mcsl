51a3cab5377757deba24b2c112b341aa24f79cbd
rubainamirza <rubainamirza@gmail.com>
2016-08-08 19:46:38 +0600

    delete deposite

--- management/delete_deposite.php
+++ management/delete_deposite.php
@@ -3,7 +3,7 @@
 <?php
 require('db_config.php');
 $id = $_GET['id'];
-$sql = "DELETE FROM deposite WHERE id = '$id'";
+$sql = "DELETE FROM deposite_withdraw WHERE id = '$id'";
 $mysqli->query($sql);
 header("Location: view_deposite.php");
 ?> 
