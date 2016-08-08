028da7a431650aad95cc032cf045e62003315c8f
shanilaBogra <soniyaislam72@gmail.com>
2016-08-08 10:47:59 +0600

    new_account.php newly added

--- management/new_account.php
+++ management/new_account.php
@@ -1,163 +1,79 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	
-	<!-- start: Meta -->
-	<meta charset="utf-8">
-	<title>KIRON - Keen Improvement Reflection For Original Needy</title>
-	<meta name="description" content="Bootstrap Metro Dashboard">
-	<meta name="author" content="Dennis Ji">
-	<meta name="keyword" content="Metro, Metro UI, Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
-	<!-- end: Meta -->
-	
-	<!-- start: Mobile Specific -->
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- end: Mobile Specific -->
-	
-	<!-- start: CSS -->
-	<link id="bootstrap-style" href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
-	<link id="base-style" href="css/style.css" rel="stylesheet">
-	<link id="base-style-responsive" href="css/style-responsive.css" rel="stylesheet">
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&subset=latin,cyrillic-ext,latin-ext' rel='stylesheet' type='text/css'>
-	<!-- end: CSS -->
-	
-
-	<!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
-	<!--[if lt IE 9]>
-	  	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<link id="ie-style" href="css/ie.css" rel="stylesheet">
-	<![endif]-->
-	
-	<!--[if IE 9]>
-		<link id="ie9style" href="css/ie9.css" rel="stylesheet">
-	<![endif]-->
-		
-	<!-- start: Favicon -->
-	<link rel="shortcut icon" href="img/favicon.ico">
-	<!-- end: Favicon -->
-	
-		
-		
-		
-</head>
-
-<body>
-		<h2>Example of using methods of Modal Plugin</h2>
-
-<!-- Button trigger modal -->
-<button class = "btn btn-primary btn-lg" data-toggle = "modal" data-target = "#myModal">
-   Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class = "modal fade" id = "myModal" tabindex = "-1" role = "dialog" 
-   aria-labelledby = "myModalLabel" aria-hidden = "true">
+<form class = "form-inline" role = "form" name="account_form" method="post" action="submit_account.php" enctype="multipart/form-data">
    
-   <div class = "modal-dialog">
-      <div class = "modal-content">
-         
-         <div class = "modal-header">
-            <button type = "button" class = "close" data-dismiss = "modal" aria-hidden = "true">
-               ×
-            </button>
-            
-            <h4 class = "modal-title" id = "myModalLabel">
-               This Modal title
-            </h4>
-         </div>
-         
-         <div class = "modal-body">
-            Press ESC button to exit.
-         </div>
-         
-         <div class = "modal-footer">
-            <button type = "button" class = "btn btn-default" data-dismiss = "modal">
-               Close
-            </button>
-            
-            <button type = "button" class = "btn btn-primary">
-               Submit changes
-            </button>
-         </div>
-         
-      </div><!-- /.modal-content -->
-   </div><!-- /.modal-dialog -->
+   <div class = "form-group">
+      <label for = "id"></label>
+      <input type = "hidden" name="id" class = "form-control" id = "id">
+   </div>
+   <div class = "form-group">
+      <label for = "name">Name:</label>
+      <input type = "text" name="name" class = "form-control" id = "name" placeholder="Enter Name">
+   </div>
+   <div class = "form-group">
+      <label for = "fathersname">Father's Name</label>
+      <input type = "text" name="fathersname" class = "form-control" id = "fathersname" placeholder = "Father's Name">
+   </div>
+   <div class = "form-group">
+      <label class = "sr-only" for = "mothersname">Mother's Name</label>
+      <input type = "text" name="mothersname" class = "form-control" id = "mothersname" placeholder = "Mother's Name">
+   </div>
+   <div class = "form-group">
+      <label class = "sr-only" for = "birthdate">Birth Date</label>
+      <input type="date" name="birthdate" class = "form-control" id = "birthdate" placeholder = "Birth Date">
+   </div>
+   <div class = "form-group">
+      <label class = "sr-only" for = "gender">Gender</label>
+      <input type="radio" name="gender" value="female">Female       
+      <input type="radio" name="gender" value="male">Male
+   </div>
+   <div class = "form-group">
+      <label class = "sr-only" for = "mobile">Mobile No</label>
+      <input type="text" name="mobile" class = "form-control" id = "mobile" placeholder = "Mobile No">
+   </div>
+   <div class = "form-group">
+      <label class = "sr-only" for = "email">E-mail</label>
+      <input type="email" name="email" class = "form-control" id = "email" placeholder = "E-mail">
+   </div>
+   <div class = "form-group">
+      <label class = "sr-only" for = "nid">NID Number</label>
+      <input type"number" name="nid" class = "form-control" id = "nid" placeholder = "NID Number">
+   </div>
+   <div class = "form-group">
+      <label class = "sr-only" for = "accountType">Account Type</label>
+      <select name="accountType" class = "form-control" id = "accountType">
+         <option value="">Select Account Type</option>
+         <option value="savings">Savings</option>
+         <option value="fixed">Fixed</option>
+
+      </select>
+   </div>
+   <div class = "form-group">
+      <label class = "sr-only" for = "primaryDeposite">Primary Deposit</label>
+      <input type = "text" name="primaryDeposite" class = "form-control" id = "primaryDeposite" placeholder = "Primary Deposit">
+   </div>
+   <div class = "form-group">
+      <label class = "sr-only" for = "address">Address</label>
+      <!--<input type = "text" name="address" class = "form-control" id = "name" placeholder = "Address">-->
+      <textarea name="address" class = "form-control" id = "address" placeholder="Enter Address"></textarea>
+   </div>
+   <div class = "form-group">
+      <label class = "sr-only" for = "brunch">Brunch</label>
+      <input type = "text" name="brunch" class = "form-control" id = "brunch" placeholder = "Brunch">
+   </div>
+   <div class = "form-group">
+      <label class = "sr-only" for = "verifier">Verifier</label>
+      <input type = "text" name="verifier" class = "form-control" id = "verifier" placeholder = "Verifier">
+   </div>
+   <div class = "form-group">
+      <label class = "sr-only" for = "nominee">Nominee</label>
+      <input type = "text" name="nominee" class = "form-control" id = "nominee" placeholder = "Nominee">
+   </div>
    
-</div><!-- /.modal -->
-
-<script>
-   $(function () { $('#myModal').modal({
-      keyboard: true
-   })});
-</script>
-	
-	<footer>
-
-		<p>
-			<span style="text-align:left;float:left">&copy; 2013 <a href="http://jiji262.github.io/Bootstrap_Metro_Dashboard/" alt="Bootstrap_Metro_Dashboard">Bootstrap Metro Dashboard</a></span>
-			
-		</p>
-
-	</footer>
-	
-	<!-- start: JavaScript-->
-
-		<script src="js/jquery-1.9.1.min.js"></script>
-	<script src="js/jquery-migrate-1.0.0.min.js"></script>
-	
-		<script src="js/jquery-ui-1.10.0.custom.min.js"></script>
-	
-		<script src="js/jquery.ui.touch-punch.js"></script>
-	
-		<script src="js/modernizr.js"></script>
-	
-		<script src="js/bootstrap.min.js"></script>
-	
-		<script src="js/jquery.cookie.js"></script>
-	
-		<script src='js/fullcalendar.min.js'></script>
-	
-		<script src='js/jquery.dataTables.min.js'></script>
-
-		<script src="js/excanvas.js"></script>
-	<script src="js/jquery.flot.js"></script>
-	<script src="js/jquery.flot.pie.js"></script>
-	<script src="js/jquery.flot.stack.js"></script>
-	<script src="js/jquery.flot.resize.min.js"></script>
-	
-		<script src="js/jquery.chosen.min.js"></script>
-	
-		<script src="js/jquery.uniform.min.js"></script>
-		
-		<script src="js/jquery.cleditor.min.js"></script>
-	
-		<script src="js/jquery.noty.js"></script>
-	
-		<script src="js/jquery.elfinder.min.js"></script>
-	
-		<script src="js/jquery.raty.min.js"></script>
-	
-		<script src="js/jquery.iphone.toggle.js"></script>
-	
-		<script src="js/jquery.uploadify-3.1.min.js"></script>
-	
-		<script src="js/jquery.gritter.min.js"></script>
-	
-		<script src="js/jquery.imagesloaded.js"></script>
-	
-		<script src="js/jquery.masonry.min.js"></script>
-	
-		<script src="js/jquery.knob.modified.js"></script>
-	
-		<script src="js/jquery.sparkline.min.js"></script>
-	
-		<script src="js/counter.js"></script>
-	
-		<script src="js/retina.js"></script>
-
-		<script src="js/custom.js"></script>
-	<!-- end: JavaScript-->
-	
-</body>
-</html>
+   <div class = "form-group">
+      <label class = "sr-only" for = "photo">File input</label>
+      <input type = "file" name="photo" id = "photo">
+   </div>
+   <div class = "form-group">
+      <input class="btn btn-primary" type="submit" name="" value="Submit">
+   </div>
+   
+</form>
