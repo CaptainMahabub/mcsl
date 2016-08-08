e85ddbcbb90ce0973905ee8296195013beddaea3
Mahabubur Rahman <mahabuburrahman.bd92@gmail.com>
2016-08-08 17:30:06 +0600

    query updated and interest amount added

--- management/submit_loan.php
+++ management/submit_loan.php
@@ -1,31 +1,23 @@
 <?php
-
-if($_SERVER['REQUEST_METHOD']=="POST"){
-	
+if($_SERVER['REQUEST_METHOD']=="POST"){	
 require('db_config.php');
-
 extract($_POST);
-
-//$password = md5($password);
+if($installment==12){$interest_amount=($loanamount*.10);}
+elseif($installment==24){$interest_amount=($loanamount*.12);}
+elseif($installment==36){$interest_amount=($loanamount*.15);}
 $date = date('Y-m-d h-i-s');
-
-
-
-
-
-$mysqli->query("INSERT INTO new_loan  VALUES
-('','$accountno', '$date', '$loanamount', '$installment', '$grantors')");
+$mysqli->query("INSERT INTO new_loan VALUES
+('','$accountno', '$date', '$loanamount', '$interest_amount', '$installment', '$grantors')");
 
 if($mysqli->affected_rows>0){
 	
-header("location:index.php");
+header("location:view_loan.php");
 
 }
 else{
 	
 	echo "<h2>Data submit not successfull</h2>";
 }
-
 }
 
 ?>
